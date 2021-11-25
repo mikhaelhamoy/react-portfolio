642e3c1d6c71e80a1f6eb365d618307ed879b7d0
Mikhael Hamoy+656+654 <mikhaelhamoy@gmail.com>
2021-11-25 04:28:49 -0500

    deploy 4.0

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,11 +7,10 @@ function About() {
       <h1>About me</h1>
       <img src={avatarImage} className="my-2" style={{ width: "25%" }} alt="cover" />
       <p>
-        Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Full Stack Web Developer
+      </p>
+      <p>
+        University of Toronto School of Continuing Studies Coding Bootcamp Graduate
       </p>
     </section>
   );
